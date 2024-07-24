29375798e79ff0188f94a4bd7bf1f81da2d857bb
arifkunc <arifkun.alif@gmail.com>
2024-07-25 05:26:49 +0700

    add starting project files

---
+++ src/main/java/com/csg/search/CsgSearchWordsApplication.java
@@ -0,0 +1,13 @@
+package com.csg.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgSearchWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsgSearchWordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csg/search/CsgSearchWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csg.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgSearchWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
